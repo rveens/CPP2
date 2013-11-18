49de16d04af36976409f936e4a8354dd524e75f2
Rick Veens <rickveens92@gmail.com>
2013-11-18 13:09:23 +0100

    Added partial functionality of ARRAY<T> class

--- CPP2week3/ARRAY.h
+++ CPP2week3/ARRAY.h
@@ -1,17 +1,90 @@
 #pragma once
 
-template<class T>
+#include <exception>
+
+template<typename T>
 class ARRAY
 {
 // public functions
 public:
-	ARRAY(sizeof initSize);
-	virtual ~ARRAY(void);
-	size_t length() const;
+	ARRAY(void) : mpData(new T[DEFAULT_SIZE]() ), mCapacity(DEFAULT_SIZE)
+	{
+		
+	}
+
+	ARRAY(size_t initSize) : mpData(new T[initSize]() ), mCapacity(initSize)
+	{
+		
+	}
+
+	virtual ~ARRAY(void)
+	{
+		delete[] this->mpData;
+	}
+
+	T &operator[](size_t position)
+	{
+		size_t t = this->capacity();
+
+		if (position < 0 || position > (this->capacity() - 1) )
+			throw std::out_of_range("ARRAY position out of range");
+
+		return this->mpData[position];
+	}
+
+	size_t length() const
+	{
+		size_t tempSizeCounter, tempCapacity;
+
+		tempSizeCounter = 0;
+		tempCapacity = this->capacity();
+
+		for (size_t i = 0; i < tempCapacity; ++i)
+			if (this->mpData[i])
+				tempSizeCounter++;
+
+		return tempSizeCounter;
+	}
+
+	size_t capacity() const
+	{
+		return mCapacity;
+	}
+
+	bool append(T newObject)
+	{
+		return false;
+		//if (this->mIndex < this->capacity()) {
+		//	for(size_t i = 0; i < this->capacity(); ++i)
+		//		if (!this->mpData[i]) {
+		//			this->mpData[this->size++];
+		//			break;
+		//		}
+		//	return true;
+		//} else if (this->mIndex == this->capacity()) { // hij is vol
+		//	 TODO maak een kopie, vernietig het oude, alloceer nieuw en zet terug.
+
+		//	 kopie maken
+		//	T *mpDataTemporaryCopy = new T[this->capacity()];
+		//	memcpy(mpDataTemporaryCopy, mpData, this->capacity());
+
+		//	 nieuwe array grootte maken.
+		//	delete[] this->mpData;
+		//	this->mpData = new T[sizeof(mpDataTemporaryCopy) + 1];
+
+		//	 oude data terug zetten
+		//	memcpy(this->mpData, mpDataTemporaryCopy, sizeof(mpDataTemporaryCopy));
+
+		//	 nieuw object toevoegen
+		//	this->mpData[this->mIndex++];
+		//} else
+		//	return false;
+	}
 
 // private members
 private:
-	T *data;
+	T *mpData;
+	size_t mCapacity;
 	static const unsigned int DEFAULT_SIZE = 1;
 };
 
--- CPP2week3/main.cpp
+++ CPP2week3/main.cpp
@@ -1,33 +1,50 @@
-#include "ARRAY.h"
+#ifdef _WIN32 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
 
-int main(int argc, char *argv[])
-{
-	return 0;
-}
+#include "ARRAY.h"
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
 
 void week3a()
 {
 	ARRAY<int> lijst(3); // maakt een array met drie elementen
-	//lijst[0] = 11;
-	//lijst[1] = 12;
-	//lijst[2] = 13;
-	//int n = lijst.length();
-	//for(int i = 0; i < n; i++)
-	//{
-	//   cout << "lijst[" << i << "]=" << lijst[i] << endl;
-	//}
-	//lijst[3] = 14; // moet een exception genereren 
+	lijst[0] = 11;
+	lijst[1] = 12;
+	lijst[2] = 13;
+	int n = lijst.length();
+	for(int i = 0; i < n; i++)
+	{
+	   cout << "lijst[" << i << "]=" << lijst[i] << endl;
+	}
+	lijst[3] = 14; // moet een exception genereren
 }
 
 void week3b()
 {
-	//ARRAY<int> lijst; // maakt een array met nul elementen
-	//lijst.append(13); // voegt toe aan het einde
-	//lijst[-1] = 12; // voegt toe aan het einde
-	//int n = lijst.length();
-	//for(int i = 0; i < n; i++)
-	//{
-	//   cout << "lijst[" << i << "]=" << lijst[i] << endl;
-	//}
-	//lijst[2] = 14; // moet een exception genereren 
+	ARRAY<int> lijst; // maakt een array met nul elementen
+	lijst.append(13); // voegt toe aan het einde
+	lijst[-1] = 12; // voegt toe aan het einde
+	int n = lijst.length();
+	for(int i = 0; i < n; i++)
+	{
+	   cout << "lijst[" << i << "]=" << lijst[i] << endl;
+	}
+	lijst[2] = 14; // moet een exception genereren 
+}
+
+int main(int argc, char *argv[])
+{
+#ifdef _WIN32
+	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+	week3a();
+	week3b();
+
+	return 0;
 }
